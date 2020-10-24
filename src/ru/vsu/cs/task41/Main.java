84417c783ca71dff32b25b2e850710322b58ff92
KandaurovIgn <72686706+KandaurovIgn@users.noreply.github.com>
2020-10-24 20:05:38 +0300

    Add files via upload

---
+++ src/ru/vsu/cs/task41/Main.java
@@ -0,0 +1,43 @@
+package task41;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double x = getNumber();
+        showResult(calculate(x));
+    }
+
+    public static double getNumber()
+    {
+     double number;
+     Scanner n1 = new Scanner(System.in);
+
+     System.out.print("Input number:");
+     number = n1.nextDouble();
+     return number;
+    }
+
+    public static double calculate(double number)
+    {
+        double result;
+        double a = 256;
+        double b = a / number * number;
+        double c = number * number + b;
+
+        while(a >= 2)
+        {
+            a = a / 2;
+            b = a / c;
+            c = number * number + b;
+        }
+        result = number / b;
+        return result;
+    }
+
+    public static void showResult(double result)
+    {
+        System.out.print("Result:" +result);
+    }
+}
