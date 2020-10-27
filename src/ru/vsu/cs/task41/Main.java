f22e71974a35a88427766f3063a4432151eb7701
KandaurovIgn <72686706+KandaurovIgn@users.noreply.github.com>
2020-10-27 21:05:40 +0300

    Update Main.java

--- src/ru/vsu/cs/task41/Main.java
+++ src/ru/vsu/cs/task41/Main.java
@@ -6,28 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         double x = getNumber();
-        showResult(calculate(x));
+        showResult(calculateY(x));
     }
 
-    public static double getNumber()
-    {
-     double number;
-     Scanner n1 = new Scanner(System.in);
+    public static double getNumber() {
+        double number;
+        Scanner n1 = new Scanner(System.in);
 
-     System.out.print("Input number:");
-     number = n1.nextDouble();
-     return number;
+        System.out.print("Input number:");
+        number = n1.nextDouble();
+        return number;
     }
 
-    public static double calculate(double number)
-    {
+    public static double calculateY(double number) {
         double result;
         double a = 256;
         double b = a / number * number;
         double c = number * number + b;
 
-        while(a >= 2)
-        {
+        while (a >= 2) {
             a = a / 2;
             b = a / c;
             c = number * number + b;
@@ -36,8 +33,7 @@ public class Main {
         return result;
     }
 
-    public static void showResult(double result)
-    {
-        System.out.print("Result:" +result);
+    public static void showResult(double result) {
+        System.out.print("Result:" + result);
     }
 }
